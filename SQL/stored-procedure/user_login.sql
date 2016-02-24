883dd92388bb1831b80da7ae353b98a17da59344
OCM-Matt <Matthew@ocicatmedia.com>
2016-02-24 13:09:42 -0500

    Update user_login.sql

--- SQL/stored-procedure/user_login.sql
+++ SQL/stored-procedure/user_login.sql
@@ -1,9 +1,9 @@
-(
-IN v_username varchar(45),
-IN v_userpass varchar(200),
-IN v_remtoken varchar(138),
-IN v_userIP varchar(39),
-OUT ret_result varchar(200)
+CREATE PROCEDURE user_login (
+	IN v_username varchar(45),
+	IN v_userpass varchar(200),
+	IN v_remtoken varchar(138),
+	IN v_userIP varchar(39),
+	OUT ret_result varchar(200)
 )
 
 BEGIN
@@ -13,17 +13,17 @@ DECLARE lv_userID INT(10) UNSIGNED;
 DECLARE lv_username VARCHAR(45);
 DECLARE lv_userrole VARCHAR(9);
 DECLARE lv_userstat VARCHAR(9);
-DECLARE lv_logintype VARCHAR(7) DEFAULT 'login';
+DECLARE lv_logintype VARCHAR(7) DEFAULT 'manual';
 
 IF v_username IS NULL THEN
-	SET lv_logintype = 'login a';
+	SET lv_logintype = 'auto';
 	
 	SELECT User_ID, User_Name, User_Role, User_Status
 	INTO lv_userID, lv_username, lv_userrole, lv_userstat
 	FROM user
-	INNER JOIN user_act
-	ON user.User_ID = user_act.User_ID
-	AND user_act.Act_Epoch = CONV(SUBSTR(v_remtoken, 129))
+	INNER JOIN user_session
+	ON user.User_ID = user_session.User_ID
+	AND user_session.Session_In = CONV(SUBSTR(v_remtoken, 129))
 	WHERE user.User_Remember = SUBSTR(v_remtoken,1,128);
 ELSE
 	SELECT User_ID, User_Name, User_Role, User_Status
@@ -48,17 +48,14 @@ ELSE
 END IF;
 
 IF lv_result THEN
-	INSERT INTO user_act (Act_Name, Act_IP, Act_Epoch, User_ID);
-	VALUES (lv_logintype, v_userIP, ZZZZZZZZZZ(NOW()), lv_userID)
+
+	INSERT INTO user_session (Session_In, Session_InType, Session_IP, User_ID)
+	VALUES (UNIX_TIMESTAMP(NOW()), lv_logintype, v_userIP, v_userID)
 	
 	IF LENGTH(v_remtoken) THEN
 		CALL user_remember (lv_userID, v_remtoken);
 	END IF;
 
-	UPDATE user
-	SET User_Online = 1
-	WHERE User_ID = v_userID;
-	
 	SET ret_result = CONCAT('welcome back *', lv_username, '*.');
 END IF;
 
