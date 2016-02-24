f0100a5eecc0ff582f3a170507b51ee4729bece9
OCM-Matt <Matthew@ocicatmedia.com>
2016-02-24 12:58:23 -0500

    Create user_session.sql

---
+++ SQL/table/user_session.sql
@@ -0,0 +1,9 @@
+CREATE TABLE user_session (
+	Session_ID BIGINT() PRIMARY KEY AUTOINCREMENT UNSIGNED NOT NULL,
+	Session_In INT(10) UNSIGNED NOT NULL,
+	Session_InType ENUM('manual','auto') NOT NULL DEFAULT 'manual',
+	Session_Out INT(10) UNSIGNED,
+	Session_OutType ENUM('manual','auto'),
+	Session_IP VARCHAR(39) NOT NULL,
+	User_ID INT(10) UNSIGNED NOT NULL
+)
