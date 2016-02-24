6c35278f372708dd8bfb4ba2219bd5b7f9d392fa
OCM-Matt <Matthew@ocicatmedia.com>
2016-02-24 13:03:54 -0500

    Update user_session.sql

--- SQL/table/user_session.sql
+++ SQL/table/user_session.sql
@@ -3,7 +3,7 @@ CREATE TABLE user_session (
 	Session_In INT(10) UNSIGNED NOT NULL,
 	Session_InType ENUM('manual','auto') NOT NULL DEFAULT 'manual',
 	Session_Out INT(10) UNSIGNED,
-	Session_OutType ENUM('manual','auto'),
+	Session_OutType ENUM('manual','auto','app'),
 	Session_IP VARCHAR(39) NOT NULL,
 	User_ID INT(10) UNSIGNED NOT NULL
 )
