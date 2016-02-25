6728bf508d9e2d539ab155660db6ba9a828a2caf
OCM-Matt <Matthew@ocicatmedia.com>
2016-02-24 22:11:31 -0500

    Update user.sql

--- DB/user/table/user.sql
+++ DB/user/table/user.sql
@@ -3,7 +3,7 @@ CREATE TABLE user (
 	User_Name varchar(45) NOT NULL,
 	User_Pass char(128) NOT NULL,
 	User_Role enum('member','admin','webmaster') NOT NULL DEFAULT 'member',
-	User_Status enum('register','acive','disable','delete') NOT NULL DEFAULT 'register',
+	User_Status enum('register','active','disabled','deleted') NOT NULL DEFAULT 'register',
 	User_Remember varchar(128) DEFAULT NULL,
 	PRIMARY KEY (User_ID),
 	UNIQUE KEY User_Name_UNIQUE (User_Name)
