2ebe9a4de771d98bdabc91511c4db991cdcecb0d
OCM-Matt <Matthew@ocicatmedia.com>
2016-02-25 12:37:37 -0500

    Update user.sql

--- DB/user/table/user.sql
+++ DB/user/table/user.sql
@@ -1,10 +1,10 @@
 CREATE TABLE user (
-	User_ID int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	User_Name varchar(45) NOT NULL,
-	User_Pass char(128) NOT NULL,
-	User_Role enum('member','admin','webmaster') NOT NULL DEFAULT 'member',
-	User_Status enum('register','active','disabled','deleted') NOT NULL DEFAULT 'register',
-	User_Remember varchar(128) DEFAULT NULL,
+	User_ID INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	User_Name VARCHAR(45) NOT NULL,
+	User_Pass CHAR(128) NOT NULL,
+	User_Role ENUM('member','admin','webmaster') NOT NULL DEFAULT 'member',
+	User_Status ENUM('register','active','disabled','deleted') NOT NULL DEFAULT 'register',
+	User_Remember VARCHAR(128) DEFAULT NULL,
 	PRIMARY KEY (User_ID),
 	UNIQUE KEY User_Name_UNIQUE (User_Name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
