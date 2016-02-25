bf954b4abc32ee61a2bae147f12b4075c2e09501
OCM-Matt <Matthew@ocicatmedia.com>
2016-02-24 20:33:30 -0500

    Update user_session.sql
    Table for tracking each unique session.
    New session start: INSERT (Out/OutType = NULL)
    Session end: UPDATE Out/OutType 
    manual = login/logout of account; auto = remember/timeout
    app = application stopped while session was active. (UPDATE WHERE Session_Out IS NULL)

--- DB/user/table/user_session.sql
+++ DB/user/table/user_session.sql
@@ -1,9 +1,10 @@
 CREATE TABLE user_session (
-	Session_ID BIGINT() PRIMARY KEY AUTOINCREMENT UNSIGNED NOT NULL,
+	Session_ID BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
 	Session_In INT(10) UNSIGNED NOT NULL,
 	Session_InType ENUM('manual','auto') NOT NULL DEFAULT 'manual',
-	Session_Out INT(10) UNSIGNED,
-	Session_OutType ENUM('manual','auto','app'),
+	Session_Out INT(10) UNSIGNED DEFAULT NULL,
+	Session_OutType ENUM('manual','auto','app') DEFAULT NULL,
 	Session_IP VARCHAR(39) NOT NULL,
-	User_ID INT(10) UNSIGNED NOT NULL
-)
+	User_ID INT(10) UNSIGNED NOT NULL,
+	PRIMARY KEY (Session_ID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
