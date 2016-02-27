e16bfa447a754d3347e2b141e9c8e19e0f0a6bc2
Matthew <Matthew@ocicatmedia.com>
2016-02-27 10:43:28 -0500

    updated logout code

--- SQL/ocm_credential/User_Logout.sql
+++ SQL/ocm_credential/User_Logout.sql
@@ -4,16 +4,22 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `User_Logout`(
 	OUT ret_result VARCHAR(200)
 )
 BEGIN
-	UPDATE user_session
-	SET Session_Out = UNIX_TIMESTAMP(NOW()), Session_OutType = v_logouttype
-	WHERE User_ID = v_userID
-    AND Session_Out IS NULL;
-    
-    IF v_logouttype = 'manual' THEN
-		UPDATE cred_user
-        SET User_Remember = NULL
-        WHERE User_ID = v_userID;
+    IF v_logouttype = 'app' THEN
+		UPDATE user_session
+		SET Session_Out = UNIX_TIMESTAMP(NOW()), Session_OutType = v_logouttype
+		WHERE Session_Out IS NULL;
+	ELSE
+		UPDATE user_session
+		SET Session_Out = UNIX_TIMESTAMP(NOW()), Session_OutType = v_logouttype
+		WHERE User_ID = v_userID
+		AND Session_Out IS NULL;
+		
+		IF v_logouttype = 'manual' THEN
+			UPDATE cred_user
+			SET User_Remember = NULL
+			WHERE User_ID = v_userID;
+		END IF;
 	END IF;
-
+    
 	SET ret_result = CONCAT_WS('|', 1, 'You have been logged out.');
 END
