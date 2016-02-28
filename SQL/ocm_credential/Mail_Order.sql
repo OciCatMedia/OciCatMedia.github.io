17450172dd8df332b15bc5bb0a2bde1d8ffcd024
Matthew <Matthew@ocicatmedia.com>
2016-02-27 22:10:28 -0500

    Updated 2 Mail_* files
    Completed/fixed Mail_Order, and finished Mail_Delete

--- SQL/ocm_credential/Mail_Order.sql
+++ SQL/ocm_credential/Mail_Order.sql
@@ -1,7 +1,7 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `Mail_Order`(
-	IN v_userID INT(10) UNSIGNED,
 	IN v_mailID INT(10) UNSIGNED,
 	IN v_mailrank TINYINT(1) UNSIGNED,
+	IN v_userID INT(10) UNSIGNED,
 	OUT ret_result VARCHAR(200)
 )
 BEGIN
@@ -17,7 +17,10 @@ BEGIN
 	
 	IF lv_mailrank IS NULL THEN
 		SET lv_result = false;
-		SET ret_result = 'That *email* isn\'t yours to modify.'; 
+		SET ret_result = 'That *email* isn\'t yours to modify.';
+	ELSEIF v_mailrank = lv_mailrank THEN
+		SET lv_result = false;
+		SET ret_result = 'The order of your *email* hasn\'t changed.';
 	END IF;
 	
 	IF lv_result THEN
@@ -29,28 +32,25 @@ BEGIN
 		IF v_mailrank > lv_mailrankmax THEN
 			SET v_mailrank = lv_mailrankmax;
 		END IF;
-			
-		IF v_mailrank = lv_mailrank THEN
-			SET lv_result = false;
-			SET ret_result = 'The order of your *email* hasn\'t changed.'; 
-		ELSE
-			IF v_mailrank > lv_mailrank THEN
-				UPDATE user_mail
-				SET Mail_Rank = Mail_Rank - 1
-				WHERE Mail_Rank > lv_mailrank
-				AND Mail_Rank <= v_mailrank;
-			ELSE 
-				UPDATE user_mail
-				SET Mail_Rank = Mail_Rank + 1
-				WHERE Mail_Rank < lv_mailrank
-				AND Mail_Rank >= v_mailrank;
-			END IF;
-			
+
+		IF v_mailrank > lv_mailrank THEN
 			UPDATE user_mail
-			SET Mail_Rank = v_mailrank
-			WHERE Mail_ID = v_mailID;
-				
-			SET ret_result = CONCAT('your *email* has been re-ordered.');
+			SET Mail_Rank = Mail_Rank - 1
+			WHERE Mail_Rank > lv_mailrank
+			AND Mail_Rank <= v_mailrank;
+		ELSE 
+			UPDATE user_mail
+			SET Mail_Rank = Mail_Rank + 1
+			WHERE Mail_Rank < lv_mailrank
+			AND Mail_Rank >= v_mailrank;
 		END IF;
+		
+		UPDATE user_mail
+		SET Mail_Rank = v_mailrank
+		WHERE Mail_ID = v_mailID;
+        
+        SET ret_result = 'your *email* has been re-ordered.';
 	END IF;
+    
+	SET ret_result = CONCAT_WS('|',lv_result, ret_result);
 END
